385ef8890dc2632a2f677b9cd37969019a1f7a6e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-21 16:35:03 -0700

    Added registration and log-in

--- server.py
+++ server.py
@@ -37,6 +37,58 @@ def user_list():
 
     return render_template("user_list.html", users=users)
 
+@app.route('/check-user', methods=["GET"])
+def check_user(email):
+    """allow a new user to register email address and password
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    print email
+    print password
+
+    if email == User.query.filter_by(email=email).first():
+        return render_template("log_in.html",
+                               email=email,
+                               password=password)
+    else:
+        return render_template("registration.html",
+                               email=email,
+                               password=password)
+
+
+@app.route('/registration', methods=["POST"])
+def register_user():
+    """allow user to register email, password, age and zipcode
+    """
+
+    """defining the user input that we are getting """
+    age = request.forms.get("age")
+    zipcode = request.forms.get("zipcode")
+    email = request.args.get("email")
+    password = request.args.get("password")
+
+    """Instantiating a new user """
+    age = User.age
+    zipcode = User.zipcode
+    email = User.email
+    password = User.password
+
+    flash("You have registered in successfully")
+
+    return render_template("log_in.html")
+
+@app.route("/log_in", methods=["POST"])
+def log_in():
+    """allow user to log_in
+    """
+
+
+
+    return render_template("log_in.html")
+
+
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
